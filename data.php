4f77b92994af11ae35a12077b49f570fc23572d3
adina1998 <97709520+adina1998@users.noreply.github.com>
2022-03-05 14:55:32 -0500

    commit

--- data.php
+++ data.php
@@ -3,13 +3,13 @@ if(isset($_POST['Color'])){
     $Color = $_POST['Color'];
     switch('Color'){
         case 'Red':
-            echo 'Your car is a NISSAN 240SX';
+            echo 'Your car is a NISSAN 240SX<br/>';
             break;
         case 'Blue':
-            echo 'Your car is a NISSAN r34';
+            echo 'Your car is a NISSAN r34<br/>';
             break;
         case 'Green':
-            echo 'Your car is a NISSAN GTI-R';
+            echo 'Your car is a NISSAN GTI-R<br/>';
             break;
 
     }
