48619f31b1a624dfd2a7a05a6fafafd45bd86df6
adina1998 <97709520+adina1998@users.noreply.github.com>
2022-03-05 13:59:18 -0500

    commit

--- data.php
+++ data.php
@@ -1,4 +1,8 @@
 <?php
+
+$Color =[
+    'red' => 'Red'
+]
 if (isset($_POST["InsertColor"]) ){
     if($_POST["InsertColor"]=="Red"){
         echo ("NISSAN 240sx")
@@ -7,7 +11,5 @@ if (isset($_POST["InsertColor"]) ){
     }
     
 }
-
-
 
 ?>
